1ec2759c521f4803c5fd0d2a22941019b5f2f39e
zahrahali1 <zahrahali2000@hotmail.com>
2024-04-29 22:38:48 +0100

    added additional notes to the index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// This code imports the CLI module from the file './lib/cli.js'
+// Then creates an instance of the CLI class and assigns it to a variable named cli and runs the method.
 const CLI = require('./lib/cli.js');
 
 const cli = new CLI();
